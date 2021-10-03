6f411657e3e55bcb159418f09e0e1ca3b51eaf14
Jakub Jungiewicz <jakub.jungiewicz@codibly.com>
2021-10-03 15:18:39 +0200

    Initial commit

---
+++ producer/src/main/java/com/codibly/producer/ProducerApplication.java
@@ -0,0 +1,11 @@
+package com.codibly.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.codibly.model")
+public class ProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+}
---
+++ producer/src/test/java/com/codibly/producer/ProducerApplicationTest.java
@@ -0,0 +1,14 @@
+package com.codibly.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProducerApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
